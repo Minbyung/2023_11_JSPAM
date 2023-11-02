14f7ef146b41dbc72ac7afde22c2034362a5bd90
Minbyung <mbmcybery@gmail.com>
2023-11-02 14:38:22 +0900

    servlet이 jsp에게 요청을 넘겨서 jsp가 응답

--- src/main/java/com/koreaIT/java/am/ArticleListServlet.java
+++ src/main/java/com/koreaIT/java/am/ArticleListServlet.java
@@ -8,13 +8,12 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/home/gugudan")
-public class gugudanServlet extends HttpServlet {
+@WebServlet("/article/list")
+public class ArticleListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	//get방식
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		request.getRequestDispatcher("/jsp/home/gugudan.jsp").forward(request, response);
+		response.getWriter().append("Hello World~!!");
 	}
 
 }
